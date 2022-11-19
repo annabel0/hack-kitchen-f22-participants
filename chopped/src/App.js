705c3a6e1753328dd6aeb764983efa65e4eea6c1
unknown <gregory06@g.ucla.edu>
2022-11-19 12:41:41 -0800

    Added image and steps

--- chopped/src/App.js
+++ chopped/src/App.js
@@ -3,7 +3,35 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h1> Hello world! </h1>
+      <h1> Bak Kut Teh Recipe </h1>
+      <img src = "https://www.unileverfoodsolutions.com.sg/dam/global-ufs/mcos/SEA/calcmenu/recipes/SG-recipes/soups/herbal-bak-kut-teh/main-header.jpg" alt = "Bak Kut Teh"></img>
+
+      <h2>Ingredients:</h2>
+      <ul>
+        <li>8 cups of water</li>
+        <li>2 tablespoons ground white pepper</li>
+        <li>1 tablespoon ground black pepper</li>
+        <li>2 tablespoons garlic powder</li>
+        <li>1 teaspoon salt</li>
+        <li>3 whole star anise</li>
+        <li>1 teaspoon ground cinnamon</li>
+        <li>20 whole garlic cloves</li>
+        <li>6 teaspoons dark soy sauce</li>
+        <li>2 pounds pork ribs, rinsed and drained, cut into 4 pieces</li>
+        <li>Cooked rice</li>
+</ul>
+      <h2>Directions:</h2>
+      <ol>
+        <li>In a large pot, add all the ingredients, except the pork ribs, and bring to a boil over high heat.</li>
+        <li>Add the pork ribs to the broth and bring back to a boil, then lower the heat and let it simmer on low for 1 hour and 15 minutes, or until the pork is tender.</li>
+        <li>Turn off the heat and cover for 10 minutes. Serve hot with rice.</li>
+      </ol>
+
+
+      
+      <footer><p>Website by Gregory, Tam, Annabel, Kaylin (Team 4)</p>
+      <p>Recipe courtesy of tastingtable.com</p></footer>
+      
     </div>
   );
 }
