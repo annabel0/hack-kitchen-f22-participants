04fdc314fea0abeef256b383e33b0e0f61ac3c10
annabel0 <118585660+annabel0@users.noreply.github.com>
2022-11-19 16:32:44 -0800

    Update getUniqueIngredients.h

--- hells-kitchen/getUniqueIngredients.h
+++ hells-kitchen/getUniqueIngredients.h
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <map>
+#include <string>
 using namespace std;
 
 int getUniqueIngredients(string ingredients[], int size) {
-    return 0;
+    map<string, int> uniqueCount;
+    for (int i = 0; i < size; i++) {
+        string ingredient = ingredients[i];
+        for (int j = 0; j < ingredients[i].length(); j++) {
+            if ((ingredients[i][j] < 65) || ((ingredients[i][j] > 90) && (ingredients[i][j] < 97)) || (ingredients[i][j] > 122)) {
+                ingredient.erase(ingredient.begin() + j);
+            }
+        }
+        
+        if (uniqueCount.find(ingredients[i]) == uniqueCount.end()) {
+            uniqueCount[ingredients[i]] = 1;
+        }
+        else {
+            uniqueCount[ingredients[i]] = uniqueCount[ingredients[i]] + 1;
+        }
+    }
+    
+    return uniqueCount.size();
 }
